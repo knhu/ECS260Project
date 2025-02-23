9525ad215b911557689f36ebb4110f81011cdadf
Jason Eissayou <92198283+Eissayou@users.noreply.github.com>
2025-02-22 17:09:26 -0800

    Made Negative Commit Percentage based on author name and project name rather than just author (where one author can be in multiple projects)

--- Satisfaction/negativeCommitPercent.py
+++ Satisfaction/negativeCommitPercent.py
@@ -1,6 +1,5 @@
 from pydriller import Repository
 import pydriller
-import pydriller
 import csv
 import pandas as pd
 from datetime import datetime
@@ -8,12 +7,11 @@ from datetime import datetime
 def checkNegativeCommitPercentage(csvFile):
     df = pd.read_csv(csvFile)
 
-    negative_commit_percentage = df.groupby('Author')['Sentiment'].apply(lambda x: (x == 'negative').sum() / len(x) * 100).reset_index(name='Negative Commit Percentage')
+    negative_commit_percentage = df.groupby(['Author', 'Project Name'])['Sentiment'].apply(lambda x: (x == 'negative').sum() / len(x) * 100).reset_index(name='Negative Commit Percentage')
 
-    df = pd.merge(df, negative_commit_percentage, on='Author', how='left')
+    df = pd.merge(df, negative_commit_percentage, on=['Author', 'Project Name'], how='left')  # Explicitly merge on both columns
 
-    df.to_csv("commit_data_with_negative_percentage.csv", index=False)  # Save to a new CSV
-
+    df.to_csv("commit_data_with_negative_percentage.csv", index=False)
 
 if __name__ == "__main__":
     checkNegativeCommitPercentage("updated_test.csv")
